66b21e6031e98cc467de49fb7f5e29c074191074
sArwocha <121029273+sArwocha@users.noreply.github.com>
2022-12-31 21:42:14 +0545

    Add files via upload

---
+++ first.java
@@ -0,0 +1,7 @@
+class first{
+    public static void main(String[] args) {
+     System.out.println("hello there i am sarwoccha poudel ");
+     System.out.println("  ");
+     System.out.println("i am trying to run java file here");
+    }
+}
